01450f68601cb52de61e7b3242c34a0f21040e39
Antonov548 <antonov5551998@gmail.com>
2024-06-04 13:52:37 +0200

    add png test

--- tests/unit_tests/image.test.js
+++ tests/unit_tests/image.test.js
@@ -43,4 +43,47 @@ describe('podofo.js', () => {
     document.delete();
     parsedDocument.delete();
   });
+
+  it('png image', async () => {
+    const Podofo = global.Podofo;
+
+    const document = new Podofo.Document();
+    const size = Podofo.getPageSize(Podofo.PageSize.A4, false);
+    const page = document.getPages()
+                            .createPage(size);
+
+    const buffer = readFile('image.png'); 
+
+    const image = new Podofo.Image(document);
+    image.loadFromBuffer(buffer);
+    
+    const painter = new Podofo.Painter();
+    painter.setCanvas(page);
+    painter.drawImage(image, 0, 0, 1, 1);
+    painter.finishDrawing();
+
+    const pdf = document.save();
+
+    const parsedDocument = new Podofo.Document();
+    parsedDocument.loadFromBuffer(pdf);
+
+    const parsedPage = parsedDocument.getPages().getPage(0);
+    const resources = parsedPage.getResources();
+
+    const objects = resources.getArray(Podofo.ResourceType.XObject);
+    expect(Object.keys(objects).length).toEqual(1);
+
+    for (const key in objects) {
+      const parsedImage = new Podofo.Image(key, objects[key]);
+      expect(parsedImage.width).toEqual(image.width);
+      expect(parsedImage.height).toEqual(image.height);
+
+      parsedImage.delete();
+    }
+
+    painter.delete();
+    image.delete();
+    document.delete();
+    parsedDocument.delete();
+  });
 });
